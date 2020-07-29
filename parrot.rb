b822a8bbf38b54e2499f31b3e0b171f316bf4adf
Javier Aguilar <coyote176@msn.com>
2020-07-29 18:15:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,7 +3,11 @@ def parrot(phrase = "Pretty bird!")
   "Squawk!"
   puts 
 end
-  
+def parrot (phrase = "Squawk!")
+  chirp = phrase
+  puts (phrase)
+  chirp
+end
 
 
 # Create method `parrot` that outputs a given phrase and
